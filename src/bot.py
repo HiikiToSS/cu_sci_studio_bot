6f954689da93a6e039ce5fb5ba61530a053eb15a
Андрей Мишурин <mishurin4@gmail.com>
2025-12-01 06:57:33 +0300

    fix: mongodb

--- src/bot.py
+++ src/bot.py
@@ -11,16 +11,13 @@ from aiogram.types import (
 from dotenv import load_dotenv
 
 from .models import Link, User, check_tg_username
-from .userdb import MongoUserDB
-
-import requests
+from .userdb import userdb
 
 load_dotenv()
 
-userdb = MongoUserDB()
-
-tkn = os.getenv("TG_BOT_TOKEN")
-TOKEN = tkn
+TOKEN = os.getenv("TG_BOT_TOKEN")
+if TOKEN is None:
+    raise Exception("Couldn't find TG_BOT_TOKEN")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -41,7 +38,7 @@ async def start_handler(message: types.Message):
     Command(BotCommand(command="get_names", description="Посмотреть на свои связи"))
 )
 async def get_usS(message: types.Message):
-    links = userdb.get_links(message.from_user.username)
+    links = await userdb.get_links(message.from_user.username)
     all_users_and_rating = "\n".join(
         # TODO: Добавить название рейтингу (Можно сделать тип из LiteralString)
         f"@{link.username_to} - {link.rating}"
@@ -93,17 +90,16 @@ async def user_name_checker(message: types.Message):
 
 @dp.callback_query(LinkCallback.filter())
 async def process_data(query: CallbackQuery, callback_data: LinkCallback):
-    if userdb.get_user(query.from_user.username) is None:
-        userdb.add_user(User(username=query.from_user.username))
-    userdb.add_link(
-        query.from_user.username,
+    from_username = query.from_user.username
+    if from_username is None:
+        raise Exception("WTF?")
+    if await userdb.get_user(from_username) is None:
+        await userdb.add_user(User(username=from_username))
+    await userdb.add_link(
+        from_username,
         Link(username_to=callback_data.username_to, rating=callback_data.rating),
     )
     await query.message.edit_text(
         f"Пасибки<3, я записал @{callback_data.username_to} в тетрадочку",
         reply_markup=None,
     )
-
-
-async def main():
-    await dp.start_polling(bot)
--- src/userdb.py
+++ src/userdb.py
@@ -1,104 +1,134 @@
+import asyncio
+import os
 from abc import ABC, abstractmethod
-from typing import Optional, List
+from typing import List, Optional
+
+import pymongo.asynchronous.collection as pymongo_collection
+import pymongo.asynchronous.database as pymongo_database
+from dotenv import load_dotenv
+from pymongo import AsyncMongoClient
+from pymongo.errors import ServerSelectionTimeoutError
 
 from .models import Link, User, Username
 
-
-from pymongo import MongoClient
-from pymongo.errors import DuplicateKeyError, ServerSelectionTimeoutError
-
-# а на кой ляд нам в принципе BaseModel? - вроде тупо класс же можно создать
-from pydantic import BaseModel
-
-import os
-from dotenv import load_dotenv
 load_dotenv()
 
-MONGO_DB_TOKEN = os.getenv('MONGO_DB_TOKEN')
-
-try:
-    client = MongoClient(MONGO_DB_TOKEN, serverSelectionTimeoutMS=5000)
-    client.admin.command("ping")
-    print("MongoDB connected successfully")
-
-except ServerSelectionTimeoutError as e:
-    print("Ошибка подключения к MongoDB:", e)
-    exit(1)
+MONGODB_HOST = os.getenv("MONGODB_HOST")
 
 
-class NewUser(BaseModel):
-    user_id: str
-    friends: List[str] = []
+class UserDB(ABC):
+    @abstractmethod
+    def add_user(self, user: User) -> None:
+        pass
+
+    @abstractmethod
+    def get_user(self, username: Username) -> Optional[User]:
+        pass
+
+    @abstractmethod
+    def get_links(self, username: Username) -> Optional[list[User]]:
+        pass
+
+    @abstractmethod
+    def add_link(self, username: Username, link: Link) -> None:
+        pass
 
 
-db = client["finik"]
-collection = db["students_connection"]
-
-
-class UserAlreadyExistsError(Exception):
+class UserNotExist(Exception):
     pass
 
 
-class MongoUserDB():
-    # я правда думал запихнуть как-то запихнуть класс и подключение монго тоже сюда: вроде так лучше, но я не уверен как делать инит, так что и ладно
+class MongoUserDB(UserDB):
+    client: Optional[AsyncMongoClient] = None
+    db: Optional[pymongo_database.AsyncDatabase] = None
+    collection: Optional[pymongo_collection.AsyncCollection] = None
 
-    def add_new_user(self, new_username: User):
-        current_user_to_add = collection.find_one({'user_id': new_username})
+    def __init__(self):
+        # Не создаем клиент в __init__, а делаем это лениво
+        pass
+
+    async def _ensure_connection(self):
+        """Ленивая инициализация подключения к MongoDB"""
+        if self.client is None:
+            try:
+                self.client = AsyncMongoClient(
+                    MONGODB_HOST, serverSelectionTimeoutMS=5000
+                )
+                self.db = self.client.get_database("cu_graph_bot")
+                self.collection = self.db.get_collection("users")
+                await self.collection.create_index("username", unique=True)
+            except ServerSelectionTimeoutError as e:
+                print("Ошибка подключения к MongoDB:", e)
+                raise e
+
+    async def add_user(self, user: User) -> None:
+        await self._ensure_connection()
+        current_user_to_add = await self.collection.find_one(
+            {"username": user.username}
+        )
         if not (current_user_to_add):
-            collection.insert_one(NewUser(user_id=new_username).model_dump())
-            print(collection.find_one({'user_id': new_username}))
+            await self.collection.insert_one(user.model_dump())
         else:
             pass
 
+    async def get_links(self, username: Username) -> List[Link]:
+        await self._ensure_connection()
+        all_friends = await self.collection.find_one({"username": username})
+        if all_friends is None:
+            raise UserNotExist
+        return [Link(i) for i in all_friends["_links"]]
 
-    def get_all_friends(self, username: Username):
-        all_friends = collection.find_one({'user_id': username})['friends']
-        return all_friends
-
-
-    def get_user(self, username: Username):
-        all_user_data = collection.find_one({'user_id': username})
+    async def get_user(self, username: Username) -> Optional[User]:
+        await self._ensure_connection()
+        all_user_data = await self.collection.find_one({"username": username})
         return all_user_data
 
-
-    def add_friend(self, username: Username, friend_to_add: Link):
-        current_user = collection.find_one({'user_id': username})
-        if friend_to_add in current_user['friends']:
-            raise UserAlreadyExistsError(f'Друг {friend_to_add} уже добавлен')
+    async def add_link(self, username: Username, link: Link) -> None:
+        await self._ensure_connection()
+        current_user = await self.collection.find_one({"username": username})
+        if current_user is None:
+            raise UserNotExist
+        for ind, ilink in enumerate(current_user.get("_links", [])):
+            if ilink["username_to"] == link.username_to:
+                await self.collection.update_one(
+                    {"username": username},
+                    {"$set": {f"_links.{ind}.rating": link.rating}},
+                )
+                return
         else:
-            collection.update_one(
-                {"user_id": username},
-                {
-                    "$push": {'friends': friend_to_add}
-                }
+            await self.collection.update_one(
+                {"username": username}, {"$push": {"_links": link.model_dump()}}
             )
 
 
-# class ListUserDB(UserDB):
-#     _users: list[User]
+class ListUserDB(UserDB):
+    _users: list[User]
 
-#     def __init__(self) -> None:
-#         self._users = []
+    def __init__(self) -> None:
+        self._users = []
 
-#     def add_user(self, user: User) -> None:
-#         if user in self._users:
-#             raise ValueError
-#         self._users.append(user)
+    def add_user(self, user: User) -> None:
+        if user in self._users:
+            raise ValueError
+        self._users.append(user)
 
-#     def get_user(self, username: Username) -> Optional[User]:
-#         for i in self._users:
-#             if i.username == username:
-#                 return i
-#         return None
+    def get_user(self, username: Username) -> Optional[User]:
+        for i in self._users:
+            if i.username == username:
+                return i
+        return None
 
-#     def get_links(self, username: Username) -> Optional[list[User]]:
-#         for i in self._users:
-#             if i.username == username:
-#                 return i._links
-#         return None
+    def get_links(self, username: Username) -> Optional[list[Link]]:
+        for i in self._users:
+            if i.username == username:
+                return i._links
+        return None
 
-#     def add_link(self, username: Username, link: Link) -> None:
-#         for i in self._users:
-#             if i.username == username:
-#                 i.set_link(link)
-#                 break
+    def add_link(self, username: Username, link: Link) -> None:
+        for i in self._users:
+            if i.username == username:
+                i.set_link(link)
+                break
+
+
+userdb = MongoUserDB()
